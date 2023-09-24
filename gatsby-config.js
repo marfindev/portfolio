60204a75c338d48842db1bdf21c0bcd20161d83a
DESKTOP-75U7EE3\Marfin <marfin.kasangke@sat.co.id>
2023-09-24 11:44:27 +0700

    Update Readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const config = require('./src/config');
 
 module.exports = {
-  pathPrefix: 'https://github.com/marfindev/portfolio',
+  pathPrefix: '/portfolio',
   siteMetadata: {
     title: 'Marfin',
     description: '',
